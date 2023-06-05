c6750352384977493af0a84e2c20d8d4852c776e
guruip <guruip@yandex.ru>
2023-06-05 22:25:47 +0300

    * add indexof.js

---
+++ indexOf.js
@@ -0,0 +1,13 @@
+const a = [1, 2, 3, 4, 5];
+const b = ['hi', 'hey'];
+
+console.log(a);
+console.log(a.indexOf(3, 3));
+
+let result = -1;
+for (let i = 0; i < ar.length; i++) {
+    if (ar[i] === item) {
+        return i;
+    }
+}
+return result;
