62e87b83f4381db09b0799c99ca89e483ba7d1fa
guruip <guruip@yandex.ru>
2023-06-11 22:04:33 +0300

    create pop and test

---
+++ pop/index.js
@@ -0,0 +1,6 @@
+const pop = (arr) => {
+    if (arr.length > 0) arr.length --;
+    return arr;
+}
+
+module.exports.pop = pop;
---
+++ pop/test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const { pop } = require('./index');
+
+describe('pop()', () => {
+    it('Длина массива изменилась', () => {
+        assert.equal(pop([1, 2, 3]).length, 2);
+    });
+    it('Длина массива не изменилась', () => {
+        assert.equal(pop([]).length, 0);
+    });
+});
