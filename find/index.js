9c8529d2ccf069d1e00d8a0eab2da31a5b3341fa
guruip <guruip@yandex.ru>
2023-06-12 12:26:21 +0300

    fix find

--- find/index.js
+++ find/index.js
@@ -1,14 +1,9 @@
 const find = (array, callback) => {
-    let element; 
-
     for (let i = 0; i < array.length; i++){
         if (callback(array[i], i, array)) {
-            element = array[i];
-            break;
+            return array[i];
         }
     }
-
-    return element;
 }
 
 module.exports.find = find;
