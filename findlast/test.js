02850c9cd4e191b0f8c254e3b3109d54dd791760
guruip <guruip@yandex.ru>
2023-06-15 10:44:56 +0300

    fix findlast test

--- findlast/test.js
+++ findlast/test.js
@@ -3,14 +3,11 @@ const { findlast } = require('./index');
 const { testArrayMethodCallback } = require('../shared/test/array-method-callback.test');
 
 describe('findlast()', () => {
-    it('Вернулся элемент удовлетворяющий условию функции callback', () => {
-        assert.equal(findlast([4, 22, 3, 40, 4], el => el > 20), 40);
-    });
     it('Элемента удовлетворяющиго условию функции callback нет', () => {
         assert.equal(findlast([4, 2, 3, 1, 4], el => false), undefined);
     });
     it('Вернулся элемент удовлетворяющий условию функции callback', () => {
         assert.equal(findlast(['a', 'b', 'c', 'b', 'd'], el => el === 'b'), 'b');
     });
-    testArrayMethodCallback(findlast);
+    testArrayMethodCallback(findlast, true);
 });
